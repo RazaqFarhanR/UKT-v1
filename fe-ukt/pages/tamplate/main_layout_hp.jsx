37d8dd4ea630639262178fdce630c2f3f6cc0be0
Dean <stevandeanachmad1@gmail.com>
2023-04-19 09:39:08 +0700

    Add : detail siswa page every ranting

---
+++ fe-ukt/pages/tamplate/main_layout_hp.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const main_layout_hp = () => {
+    return (
+        <>
+            <div className="font-lato">
+
+                {/* awal wrapper konten utama */}
+                <div className="w-full h-screen">
+
+                    {/* header */}
+                    <Header />
+                    {/* akhir header */}
+
+                    {/* konten utama */}
+                    <div className="min-h-full bg-darkBlue px-10 py-8">
+                        
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default main_layout_hp
