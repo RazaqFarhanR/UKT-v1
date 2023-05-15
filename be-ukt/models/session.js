5c6767c7d2520fae23bbd2e763d52b79154802ad
RazaqFarhanR <aljawadfarhan@gmail.com>
2023-05-15 17:02:31 +0700

    upd: simpan jawaban keSHan

--- be-ukt/models/session.js
+++ be-ukt/models/session.js
@@ -15,6 +15,14 @@ module.exports = (sequelize, DataTypes) => {
         foreignKey: "id_lembar_soal",
         as: "lembar_session"
       })
+      this.belongsTo(models.siswa, {
+        foreignKey: "id_siswa",
+        as: "keshan_siswa"
+      })
+      this.hasMany(models.lembar_jawaban, {
+        foreignKey: "id_session",
+        as: "lembar_jawaban"
+      })
     }
   }
   session.init({
@@ -26,8 +34,11 @@ module.exports = (sequelize, DataTypes) => {
     },
     id_lembar_soal: DataTypes.INTEGER,
     id_siswa: DataTypes.INTEGER,
+    id_event: DataTypes.INTEGER,
     nilai: DataTypes.INTEGER,
-    waktu_pengerjaan: DataTypes.INTEGER
+    waktu_pengerjaan: DataTypes.INTEGER,
+    start: DataTypes.DATE,
+    finish: DataTypes.DATE
   }, {
     sequelize,
     modelName: 'session',
